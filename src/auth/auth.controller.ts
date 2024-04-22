56a9bb0bfec367580d9d3664bb6cb55fc82177f8
Weeraphat2000 <weeraphat.main@gmail.com>
2024-04-22 15:05:41 +0700

    http status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
 import {
   Controller,
   Get,
+  Headers,
+  HttpStatus,
   MiddlewareConsumer,
   Post,
   Request,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
@@ -12,6 +15,7 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LocalAuthGuard } from './local/local-auth.guards';
 import { Authenticate } from './authenticate.middleware';
 import { TestMiddleware } from 'src/test.middleware';
+import { Response } from 'express';
 
 @Controller('auth')
 // @UseGuards(Authenticate)
@@ -27,8 +31,21 @@ export class AuthController {
   }
 
   @Get()
-  getuser(@Request() req: any) {
+  getuser(@Request() req: any, @Headers() headers: { authorization: string }) {
+    console.log(headers.authorization, 'headers');
     console.log(req.user, 'asdfasdf');
     return 'asdf';
+  }
+
+  @Get('/res')
+  testRes(@Res() res: Response) {
+    // HttpStatus.OK 200
+    // HttpStatus.CREATED 201
+    // HttpStatus.ACCEPTED 202
+    // HttpStatus.BAD_REQUEST 400
+    // HttpStatus.UNAUTHORIZED 401
+    // HttpStatus.NOT_FOUND 404
+    // HttpStatus.INTERNAL_SERVER_ERROR 500
+    return res.status(HttpStatus.OK).json({ message: 'OK' });
   }
 }
